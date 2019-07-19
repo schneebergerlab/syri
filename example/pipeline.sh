c38b916029c029ed8f7483d8f8f0688b66fe458c
Manish Goel <goel@pc-t7-095.mpipz.mpg.de>
2019-07-19 12:20:19 +0200

    updated example pipeline

--- example/pipeline.sh
+++ example/pipeline.sh
@@ -1,10 +1,13 @@
+cwd="."     # Change to working directory
+cd $cwd
+PATH_TO_SYRI="../syri/bin/syri"
 ## Get Yeast Reference genome
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/146/045/GCA_000146045.2_R64/GCA_000146045.2_R64_genomic.fna.gz
-gzip -d GCA_000146045.2_R64_genomic.fna.gz
+gzip -df GCA_000146045.2_R64_genomic.fna.gz
 
 ## Get Query genome
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/977/955/GCA_000977955.2_Sc_YJM1447_v1/GCA_000977955.2_Sc_YJM1447_v1_genomic.fna.gz
-gzip -d GCA_000977955.2_Sc_YJM1447_v1_genomic.fna.gz
+gzip -df GCA_000977955.2_Sc_YJM1447_v1_genomic.fna.gz
 
 ## Remove mitochondria
 head -151797 GCA_000977955.2_Sc_YJM1447_v1_genomic.fna > GCA_000977955.2_Sc_YJM1447_v1_genomic.fna.filtered
@@ -20,13 +23,13 @@ head -151797 GCA_000977955.2_Sc_YJM1447_v1_genomic.fna > GCA_000977955.2_Sc_YJM1
 ## OF CHROMOSOMES.
 
 ## Perform whole genome alignment
-ln -s GCA_000146045.2_R64_genomic.fna refgenome
-ln -s GCA_000977955.2_Sc_YJM1447_v1_genomic.fna.filtered qrygenome
+ln -sf GCA_000146045.2_R64_genomic.fna refgenome
+ln -sf GCA_000977955.2_Sc_YJM1447_v1_genomic.fna.filtered qrygenome
 nucmer --maxmatch -c 100 -b 500 -l 50 refgenome qrygenome       # Whole genome alignment. Any other alignment can also be used.
-delta-filter -m -i 90 -l 100 out.delta > out.delta              # Remove small and lower quality alignments
-show-coords -THrd out.delta > out.coords                        # Convert alignment information to a .TSV format as required by SyRI
+delta-filter -m -i 90 -l 100 out.delta > out.filtered.delta              # Remove small and lower quality alignments
+show-coords -THrd out.filtered.delta > out.filtered.coords                        # Convert alignment information to a .TSV format as required by SyRI
 
 ## Ryn SyRI 
-python3 /biodata/dep_coupland/grp_schneeberger/projects/SynSearch/scripts/python/syri/syri/bin/syri -c out.coords -d out.delta -r refgenome -q qrygenome
+python3 $PATH_TO_SYRI -c out.filtered.coords -d out.filtered.delta -r refgenome -q qrygenome
 
 #OUTPUT: SyRI would report genomic structural differences in syri.out and syri.vcf.
