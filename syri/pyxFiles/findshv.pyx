166b020b31cfa30be4897b888090b9f7c2e79a36
Manish Goel <mnshgl0110@gmail.com>
2021-03-12 14:59:29 +0100

    Changed how data is parsed to show-snps. Earlier methods was causing errors when chromosome IDs were too long

--- syri/pyxFiles/findshv.pyx
+++ syri/pyxFiles/findshv.pyx
@@ -151,7 +151,7 @@ def getshv(args, coords, chrlink):
         global chrsnps                          # Use global variable to save SNPs divided based on chromosomes. Using global variable, saves memory when classifying snps/indels using parallel processing
 
         allAlignments = readSRData(cwdpath, prefix, args.all)
-        mapit = 0
+        mapit = 0conda i
         if os.path.isfile(cwdpath+prefix+"mapids.txt"):
             mapit = 1
             chroms = {}
